ba1213cfaada5d7db335b16f95aa8b0af49f8af4
rahmatrhd <kickyou44@gmail.com>
2019-06-24 19:17:34 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function textTransformator(text) {
+  // your code here
+}
+
+// TEST CASE:
+console.log(textTransformator('hello world')) // h3ll0 w0rld
+console.log(textTransformator('i love you 3000')) // 1 l0v3 y0u 3000
+console.log(textTransformator('do not repeat yourself')) // d0 n0t r3p34t y0urs3lf
