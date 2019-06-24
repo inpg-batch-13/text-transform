e984bcca75009418deba42b96f197027f65e6597
felishaww <felishamiranda1510@gmail.com>
2019-06-24 19:44:12 +0700

    done

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 function textTransformator(text) {
   // your code here
+  var result = []
+
+  for (var i = 0; i < text.length; i++){
+
+    if (text[i] === 'a'|| text[i] == 'A'){
+      //text[i] = 4
+      result.push(4)
+    }
+    else if (text[i] == 'i' || text[i] == 'I'){
+      //text[i] = 1
+      result.push(1)
+    }
+    else if (text[i] == 'e'|| text[i] == 'E'){
+      //text[i] = 3
+      result.push(3)
+    }
+    else if (text[i] == 'o' || text[i] == 'O'){
+      //text[i] = 0
+      result.push(0)
+    }
+   else {
+     result.push(text[i])
+    }
+
+  }
+  
+  return result.join('')
 }
 
 // TEST CASE:
